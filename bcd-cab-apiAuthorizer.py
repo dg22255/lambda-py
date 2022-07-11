074cc868ca1eb84db6a32a4705119ac4b53a5780
dg22255 <Leann.bernard@tn.gov>
2022-07-11 12:58:42 -0500

    Update bcd-cab-apiAuthorizer.py
    added the other resources to the code

--- bcd-cab-apiAuthorizer.py
+++ bcd-cab-apiAuthorizer.py
@@ -1,18 +1,64 @@
 def lambda_handler(event, context):
     print(event)
     auth="Deny"
-    if event['authorizationToken'] == 'abcdefghijklmnopqrstuvwxyzBogusToken0123456789':auth="Allow" 
-    authResponse = { 
-        "principalId": "user",
-        "policyDocument":{
-            "Version":"2012-10-17",
-            "Statement":[
-                {
-                    "Action":"execute-api:Invoke",
-                    "Resource":"arn:aws:execute-api:us-east-1:041407107837:wqtoft7ru3/*/GET/parmlist",
-                    "Effect":auth
-                }
-            ]
+    if event['Resource'] == 'GET':
+        if event['authorizationToken'] == 'abcdefghijklmnopqrstuvwxyzBogusToken0123456789':auth="Allow" 
+        authResponse = { 
+            "principalId": "user",
+            "policyDocument":{
+                "Version":"2012-10-17",
+                "Statement":[
+                    {
+                        "Action":"execute-api:Invoke",
+                        "Resource":"arn:aws:execute-api:us-east-1:041407107837:wqtoft7ru3/*/GET/*",
+                        "Effect":auth
+                    }
+                ]
+            }
         }
-    }
+    elif event['Resource'] == 'DELETE':
+        if event['authorizationToken'] == 'abcdefghijklmnopqrstuvwxyzBogusToken0123456789':auth="Allow" 
+        authResponse = { 
+            "principalId": "user",
+            "policyDocument":{
+                "Version":"2012-10-17",
+                "Statement":[
+                    {
+                        "Action":"execute-api:Invoke",
+                        "Resource":"arn:aws:execute-api:us-east-1:041407107837:wqtoft7ru3/*/DELETE/*",
+                        "Effect":auth
+                    }
+                ]
+            }
+        }    
+    elif event['Resource'] == 'POST':
+        if event['authorizationToken'] == 'abcdefghijklmnopqrstuvwxyzBogusToken0123456789':auth="Allow" 
+        authResponse = { 
+            "principalId": "user",
+            "policyDocument":{
+                "Version":"2012-10-17",
+                "Statement":[
+                    {
+                        "Action":"execute-api:Invoke",
+                        "Resource":"arn:aws:execute-api:us-east-1:041407107837:wqtoft7ru3/*/POST/*",
+                        "Effect":auth
+                    }
+                ]
+            }
+        }
+    else:
+        if event['authorizationToken'] == 'abcdefghijklmnopqrstuvwxyzBogusToken0123456789':auth="Allow" 
+        authResponse = { 
+            "principalId": "user",
+            "policyDocument":{
+                "Version":"2012-10-17",
+                "Statement":[
+                    {
+                        "Action":"execute-api:Invoke",
+                        "Resource":"arn:aws:execute-api:us-east-1:041407107837:wqtoft7ru3/*/PATCH/*",
+                        "Effect":auth
+                    }
+                ]
+            }
+        }
     return authResponse
